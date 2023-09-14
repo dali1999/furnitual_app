d58835dc2a5386b8c82148a00689804c72ff2966
Jules <dali1999@naver.com>
2023-09-14 23:43:12 +0900

    하단 네비게이션 바 제작

---
+++ screens/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import styles from "./home.style";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+export default Home;
---
+++ screens/home.style.js
@@ -0,0 +1,6 @@
+import { StyleSheet } from "react-native/types";
+import { COLORS, SIZES } from "../components/index";
+
+const styles = StyleSheet.create({});
+
+export default styles;
