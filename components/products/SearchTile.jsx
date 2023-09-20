126f8daea785b6c8efcf5d6555d684edff62c99b
Jules <dali1999@naver.com>
2023-09-21 06:42:25 +0900

    feat: search 시 해당 products 보여주는 Flatlist 추가

---
+++ components/products/SearchTile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import styles from "./searchTile.style";
+import { useNavigation } from "@react-navigation/native";
+
+export default function SearchTile({ item }) {
+  const navigation = useNavigation();
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.container}
+        onPress={() => navigation.navigate("ProductDetails", { item })}
+      >
+        <View style={styles.image}>
+          <Image source={{ uri: item.imageUrl }} style={styles.productImg} />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.productTitle}>{item.title}</Text>
+          <Text style={styles.supplier}>{item.supplier}</Text>
+          <Text style={styles.price}>{item.price}</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,10 +1,12 @@
-import { TextInput, View, Text, TouchableOpacity } from "react-native";
+import { TextInput, View, Text, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import styles from "./search.style";
 import { COLORS, SIZES } from "../constants";
 import axios from "axios";
+import { FlatList } from "react-native-gesture-handler";
+import SearchTile from "../components/products/SearchTile";
 
 const Search = () => {
   const [searchKey, setSearchKey] = useState("");
@@ -49,6 +51,21 @@ const Search = () => {
           </TouchableOpacity>
         </View>
       </View>
+      {searchResults.length === 0 ? (
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("../assets/images/Pose23.png")}
+            style={styles.searchImg}
+          />
+        </View>
+      ) : (
+        <FlatList
+          data={searchResults}
+          keyExtractor={(item) => item._id}
+          renderItem={({ item }) => <SearchTile item={item} />}
+          style={{ marginHorizontal: 12 }}
+        />
+      )}
     </SafeAreaView>
   );
 };
