c2f1dfabca4b18ee115d5014f189a3df865075c0
Jules <dali1999@naver.com>
2023-09-23 03:41:41 +0900

    feat:  LoginPage

--- components/index.js
+++ components/index.js
@@ -2,5 +2,7 @@ import Welcome from "./home/Welcome";
 import ProductCardview from "./products/ProductCardView";
 import ProductRow from "./products/ProductRow";
 import ProductLists from "./products/ProductLists";
+import BackBtn from "./BackBtn";
+import Button from "./Button";
 
-export { Welcome, ProductCardview, ProductRow, ProductLists };
+export { Welcome, ProductCardview, ProductRow, ProductLists, BackBtn, Button };
--- screens/LoginPage.jsx
+++ screens/LoginPage.jsx
@@ -1,11 +1,181 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  SafeAreaView,
+  TextInput,
+  Alert,
+} from "react-native";
+import {
+  AntDesign,
+  MaterialCommunityIcons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
 import styles from "./login.style";
+import { BackBtn, Button } from "../components";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { COLORS } from "../constants";
 
-export default function LoginPage() {
+const validationSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(8, "Password must be ar least 8 character")
+    .required("Required"),
+  email: Yup.string()
+    .email("Provide a valid email address")
+    .required("Required"),
+});
+
+export default function LoginPage({ navigation }) {
+  const [loader, setLoader] = useState(false);
+  const [responseData, setResponseData] = useState(null);
+  const [obsecureText, setObsecureText] = useState(false);
+
+  const inValidForm = () => {
+    Alert.alert("Invalid Form", "Please provide all required fields", [
+      {
+        text: "Cancel",
+        onPress: () => {},
+      },
+      {
+        text: "Continue",
+        onPress: () => {},
+      },
+      { defaultIndex: 1 },
+    ]);
+  };
+
   return (
-    <View>
-      <Text>LOGIN PAGE</Text>
-    </View>
+    <ScrollView>
+      <SafeAreaView style={{ marginHorizontal: 20, marginVertical: 40 }}>
+        <View>
+          <BackBtn onPress={() => navigation.goBack()} />
+          <Image
+            source={require("../assets/images/bk.png")}
+            style={styles.cover}
+          />
+
+          <Text style={styles.title}>Unlimited Luxurious Furniture</Text>
+
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => console.log(values)}
+          >
+            {({
+              handleChange,
+              handleBlur,
+              touched,
+              handleSubmit,
+              values,
+              errors,
+              isValid,
+              setFieldTouched,
+            }) => (
+              <View>
+                {/* EMAIL =============================================*/}
+                <View style={styles.wrapper}>
+                  <Text style={styles.label}>Email</Text>
+                  <View
+                    style={styles.inputWrapper(
+                      touched.email ? COLORS.primary : COLORS.offwhite
+                    )}
+                  >
+                    <MaterialCommunityIcons
+                      name="email-outline"
+                      size={20}
+                      color={COLORS.gray}
+                      style={styles.iconStyle}
+                    />
+                    <TextInput
+                      placeholder="Enter email"
+                      onFocus={() => {
+                        setFieldTouched("email");
+                      }}
+                      onBlur={() => {
+                        setFieldTouched("email", "");
+                      }}
+                      value={values.email}
+                      onChangeText={handleChange("email")}
+                      autoCapitalize="none"
+                      antoCorrect="false"
+                      style={{ flex: 1 }}
+                    />
+                  </View>
+                  {touched.email && errors.email && (
+                    <Text style={styles.errorMessage}>{errors.email}</Text>
+                  )}
+                </View>
+
+                {/* PASSWORD =============================================*/}
+                <View style={styles.wrapper}>
+                  <Text style={styles.label}>Password</Text>
+                  <View
+                    style={styles.inputWrapper(
+                      touched.password ? COLORS.primary : COLORS.offwhite
+                    )}
+                  >
+                    <MaterialCommunityIcons
+                      name="lock-outline"
+                      size={20}
+                      color={COLORS.gray}
+                      style={styles.iconStyle}
+                    />
+                    <TextInput
+                      secureTextEntry={obsecureText}
+                      placeholder="Password"
+                      onFocus={() => {
+                        setFieldTouched("password");
+                      }}
+                      onBlur={() => {
+                        setFieldTouched("password", "");
+                      }}
+                      value={values.password}
+                      onChangeText={handleChange("password")}
+                      autoCapitalize="none"
+                      antoCorrect="false"
+                      style={{ flex: 1 }}
+                    />
+
+                    <TouchableOpacity
+                      onPress={() => {
+                        setObsecureText(!obsecureText);
+                      }}
+                    >
+                      <MaterialCommunityIcons
+                        name={obsecureText ? "eye-outline" : "eye-off-outline"}
+                        size={18}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                  {touched.password && errors.password && (
+                    <Text style={styles.errorMessage}>{errors.password}</Text>
+                  )}
+                </View>
+
+                {/* LOGIN BUTTON =============================================*/}
+                <Button
+                  title={"L O G I N"}
+                  onPress={isValid ? handleSubmit : inValidForm}
+                  isValid={isValid}
+                />
+
+                <Text
+                  style={styles.registration}
+                  onPress={() => {
+                    navigation.navigate("SignUp");
+                  }}
+                >
+                  Register
+                </Text>
+              </View>
+            )}
+          </Formik>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
