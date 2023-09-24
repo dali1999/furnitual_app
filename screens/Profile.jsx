6ac16ad80923f8135e0643f90ab03faf6febca10
Jules <dali1999@naver.com>
2023-09-25 05:12:17 +0900

    feat: 유저 정보 확인 function

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -9,10 +9,33 @@ import {
 import styles from "./profile.style";
 import { StatusBar } from "expo-status-bar";
 import { COLORS } from "../constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Profile = ({ navigation }) => {
   const [userData, setUserData] = useState(null);
   const [userLogin, setUserLogin] = useState(false);
+
+  useEffect(() => {
+    checkExistingUser();
+  });
+  const checkExistingUser = async () => {
+    const id = await AsyncStorage.getItem("id");
+    const useId = `user${JSON.parse(id)}`;
+
+    try {
+      const currentUser = await AsyncStorage.getItem(useId);
+
+      if (currentUser != null) {
+        const parsedData = JSON.paese(currentUser);
+        setUserData(parsedData);
+        setUserLogin(true);
+      } else {
+        navigation.navigate("Login");
+      }
+    } catch (error) {
+      console.log("Error retrieving the data:", error);
+    }
+  };
 
   const logout = () => {
     Alert.alert("Logout", "Are you sure you want to", [
