03d2157a9d3b66fb6117fcbb5de4e4d71014caab
Jules <dali1999@naver.com>
2023-09-25 05:34:12 +0900

    feat: Home 에서 user 정보를 통해 location 정보 받기

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons, Fontisto } from "@expo/vector-icons";
@@ -7,15 +7,42 @@ import { Welcome } from "../components";
 import Carousel from "../components/home/Carousel";
 import Headings from "../components/home/Headings";
 import ProductRow from "../components/products/ProductRow";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Home = () => {
+  const [userData, setUserData] = useState(null);
+  const [userLogin, setUserLogin] = useState(false);
+
+  useEffect(() => {
+    checkExistingUser();
+  }, []);
+
+  const checkExistingUser = async () => {
+    const id = await AsyncStorage.getItem("id");
+    const useId = `user${JSON.parse(id)}`;
+
+    try {
+      const currentUser = await AsyncStorage.getItem(useId);
+
+      if (currentUser !== null) {
+        const parsedData = JSON.parse(currentUser);
+        setUserData(parsedData);
+        setUserLogin(true);
+      }
+    } catch (error) {
+      console.log("Error retrieving the data:", error);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.appBarWrapper}>
         <View style={styles.appBar}>
           <Ionicons name="location-outline" size={24} />
 
-          <Text style={styles.location}>Seoul Korea</Text>
+          <Text style={styles.location}>
+            {userData ? userData.location : "Default"}
+          </Text>
 
           <View style={{ alignItems: "flex-end" }}>
             <View style={styles.cartCount}>
